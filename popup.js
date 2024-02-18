a4edd2e459d476d8c7d85a421b8ab78df84f0638
Malika17 <malikasharma1711@gmail.com>
2024-02-18 14:22:15 +0530

    first commit

---
+++ popup.js
@@ -0,0 +1,57 @@
+document.addEventListener("DOMContentLoaded", function () {});
+
+//STATE VARIABLE
+let providerURL = "";
+let provider;
+let address;
+
+//FUNCTION
+function handler() {}
+
+function checkBalance() {}
+
+function getOpenNetwork() {}
+
+function getSelectedNetwork() {}
+
+function setNetwork() {}
+
+function loginUser() {}
+
+function createUser() {}
+
+function openCreate() {}
+
+function signUp() {}
+
+function login() {}
+
+function logout() {}
+
+function openTransfer() {}
+
+function goBack() {}
+
+function openImport() {}
+
+function importGoBack() {}
+
+function openActivity() {}
+
+function openAssests() {}
+
+function goHomePage() {}
+
+function openImportModel() {}
+
+function closeImportModel() {}
+
+function addToken() {}
+
+function addAccount() {}
+
+function myFunction() {}
+
+function copyAddress() {}
+
+function changeAddress() {}
